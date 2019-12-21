8dddaa600759a5f7c9bdf9a37132d9bf8bcb84be
eddy <379953356@qq.com>
2019-12-22 00:13:10 +0800

    添加多模型bagging能力

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# 深圳车牌价格预测
-* 2015-2019年竞价数据预处理（preprocess.py）
-* 通过sk-learning进行回归预测（main.py）
+# TODO
--- main.py
+++ main.py
@@ -5,6 +5,7 @@
     参考：https://www.jianshu.com/p/97df66c3a3f8
 '''
 import pandas as pd
+import numpy as np
 from sklearn import linear_model
 from sklearn.svm import SVR
 from sklearn.neighbors import KNeighborsRegressor
@@ -32,8 +33,27 @@ for index, element in pdf.iteritems():
     if 'Unnamed' in str(index):
         continue
 
+        # # 去除最后一个（2019年11月），用来预测
+        # if index==57:
+        #     continue
+
+        # # 去除导数第二个（2019年10月），用来预测
+        # if index == 56:
+        #     continue
+
+    # # 去除导数第三个（2019年9月），用来预测
+    # if index == 55:
+    #     continue
+
+    # 去除导数第四个（2019年8月），用来预测
+    if index == 54:
+        continue
+
     l_element = element.tolist()
-    X.append(l_element[0:6])
+    # 全部特征
+    # X.append(l_element[0:6])
+    # 只使用前两次竞价价格
+    X.append(l_element[4:6])
     Y1.append(l_element[6])
     Y2.append(l_element[7])
 
@@ -41,25 +61,74 @@ print(X)
 print(Y1)
 print(Y2)
 
+l_models = list()
 # 拟合误差非常大
-# model = linear_model.LinearRegression()
-# model=SVR(kernel='linear')
+model = linear_model.LinearRegression()
+l_models.append(model)
+model = SVR(kernel='linear')
+l_models.append(model)
 # 运行速度非常慢（半天跑不完）
 # model=SVR(kernel="poly")
-# 误差很大
+# 拟合误差很大
+model = SVR(kernel="rbf")
+l_models.append(model)
+model = KNeighborsRegressor(weights="uniform")
+l_models.append(model)
+# 完全拟合
+model = DecisionTreeRegressor()
+l_models.append(model)
+model = RandomForestRegressor()
+l_models.append(model)
+# 完全拟合
+model = ExtraTreesRegressor()
+l_models.append(model)
+# 拟合比较接近
+model = GradientBoostingRegressor()
+l_models.append(model)
+
+###### 测试单个模型####
+# model = linear_model.LinearRegression()
+# model=SVR(kernel='linear')
 # model=SVR(kernel="rbf")
 # model=KNeighborsRegressor(weights="uniform")
-# 完全拟合
 # model=DecisionTreeRegressor()
 # model=RandomForestRegressor()
-# 完全拟合
 # model=ExtraTreesRegressor()
-# 比较接近
 model = GradientBoostingRegressor()
 
+# 待预测特征
+# 2019.08
+x = [[23151, 25107]]
+# # 2019.09
+# x = [[26412, 28139]]
+# # 2019.10特征
+# x = [[28871, 30719]]
+# # 2019.11特征
+# x=[[29060, 31156]]
+# 待预测最低价
 model.fit(X, Y1)
+y1 = model.predict(x)
+# 待预测平均价
+model.fit(X, Y2)
+y2 = model.predict(x)
+print('---------单个模型-----------')
+print('预测最低价格：{}'.format(y1))
+print('预测均值价格:{}'.format(y2))
 
-predic_x = [[2015, 1, 3912, 1578, 15522, 17021], [2015, 2, 6491, 742, 12385, 12637],
-            [2015, 3, 9878, 10281, 11225, 11341], [2015, 4, 3880, 10646, 11198, 11426]]
-predic_y = model.predict(predic_x)
-print(predic_y)
+####### 测试所有模型bagging #########
+l_y1 = list()
+l_y2 = list()
+for m in l_models:
+    m.fit(X, Y1)
+    y1 = m.predict(x)
+    l_y1.append(y1)
+
+    m.fit(X, Y2)
+    y2 = m.predict(x)
+    l_y2.append(y2)
+
+y1 = np.mean(l_y1)
+y2 = np.mean(l_y2)
+print('---------多模型bagging-----------')
+print('预测最低价格：{}'.format(y1))
+print('预测均值价格:{}'.format(y2))
