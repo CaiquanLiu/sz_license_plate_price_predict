8fee7426a4ad5268aa265a8522102a1946aaeb7f
eddy <379953356@qq.com>
2020-01-15 17:36:02 +0800

    完成代码整理

--- config.py
+++ config.py
@@ -2,5 +2,7 @@
 '''
 全局配置
 '''
+# 网页公布的历史数据
 web_data_file = r'data/rawdata'
+# 整理好的excel文件格式数据
 excel_file = r'data/format_data.xlsx'
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 '''
 模型代码：
-    参考：https://blog.csdn.net/hubingshabi/article/details/80172608
     参考：https://www.jianshu.com/p/97df66c3a3f8
 '''
 import pandas as pd
@@ -16,38 +15,34 @@ from sklearn.ensemble import GradientBoostingRegressor
 
 import config
 
+# 训练样本处理
 pdf = pd.read_excel(config.excel_file)
-
-# print(type(pdf[0]))
-
 X = list()
 # 最低价
 Y1 = list()
 # 均价
 Y2 = list()
 
-# print(type(pdf))
-
 for index, element in pdf.iteritems():
     # 去除第一列（字段标识）
     if 'Unnamed' in str(index):
         continue
 
-        # # 去除最后一个（2019年11月），用来预测
-        # if index==57:
-        #     continue
+    # # 去除最后一个（2019年11月），用来验证预测
+    # if index==57:
+    #     continue
 
-        # # 去除导数第二个（2019年10月），用来预测
-        # if index == 56:
-        #     continue
+    # # 去除导数第二个（2019年10月），用来验证预测
+    # if index == 56:
+    #     continue
 
-    # # 去除导数第三个（2019年9月），用来预测
+    # # 去除导数第三个（2019年9月），用来验证预测
     # if index == 55:
     #     continue
 
-    # 去除导数第四个（2019年8月），用来预测
-    if index == 54:
-        continue
+    # # 去除导数第四个（2019年8月），用来验证预测
+    # if index == 54:
+    #     continue
 
     l_element = element.tolist()
     # 全部特征
@@ -57,54 +52,54 @@ for index, element in pdf.iteritems():
     Y1.append(l_element[6])
     Y2.append(l_element[7])
 
-print(X)
-print(Y1)
-print(Y2)
+# print(X)
+# print(Y1)
+# print(Y2)
 
+# 多模型初始化
 l_models = list()
-# 拟合误差非常大
 model = linear_model.LinearRegression()
 l_models.append(model)
 model = SVR(kernel='linear')
 l_models.append(model)
 # 运行速度非常慢（半天跑不完）
 # model=SVR(kernel="poly")
-# 拟合误差很大
 model = SVR(kernel="rbf")
 l_models.append(model)
 model = KNeighborsRegressor(weights="uniform")
 l_models.append(model)
-# 完全拟合
+# 树模型中，random_state参数会导致结果波动
 model = DecisionTreeRegressor()
 l_models.append(model)
 model = RandomForestRegressor()
 l_models.append(model)
-# 完全拟合
 model = ExtraTreesRegressor()
 l_models.append(model)
-# 拟合比较接近
 model = GradientBoostingRegressor()
 l_models.append(model)
 
-###### 测试单个模型####
+# 测试单个模型
 # model = linear_model.LinearRegression()
 # model=SVR(kernel='linear')
 # model=SVR(kernel="rbf")
 # model=KNeighborsRegressor(weights="uniform")
+# 树模型中，random_state参数会导致结果波动
 # model=DecisionTreeRegressor()
 # model=RandomForestRegressor()
 # model=ExtraTreesRegressor()
 model = GradientBoostingRegressor()
 
-# 待预测特征
-# 2019.08
-x = [[23151, 25107]]
+# 待预测特征（实际预测时，只要初始化特征x即可）
+# # 2019.08
+# x = [[23151, 25107]]
 # # 2019.09
 # x = [[26412, 28139]]
 # # 2019.10特征
 # x = [[28871, 30719]]
 # # 2019.11特征
 # x=[[29060, 31156]]
+# # 2019.12
+x = [[24563, 26085]]
 # 待预测最低价
 model.fit(X, Y1)
 y1 = model.predict(x)
@@ -115,9 +110,10 @@ print('---------单个模型-----------')
 print('预测最低价格：{}'.format(y1))
 print('预测均值价格:{}'.format(y2))
 
-####### 测试所有模型bagging #########
+# 测试所有模型bagging
 l_y1 = list()
 l_y2 = list()
+
 for m in l_models:
     m.fit(X, Y1)
     y1 = m.predict(x)
